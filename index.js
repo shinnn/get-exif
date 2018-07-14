219c1700023661f27cdbb3037f9cf99c6bc53c60
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-07-14 19:42:57 +0900

    merge ERR_EMPTY_DATA error with ERR_INSUFFICIENT_DATA_SIZE

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const {load} = require('piexifjs');
 const inspectWithKind = require('inspect-with-kind');
 const isJpg = require('is-jpg');
 
+const MINIMUM_JPEG_SIZE = 107;
 const ERROR = 'Expected a Buffer of JPEG or a Buffer-to-latin1 encoded string of it';
-const SIZE_ERROR = 'JPEG must be 107 bytes or more.';
+const SIZE_ERROR = `JPEG must be ${MINIMUM_JPEG_SIZE} bytes or more.`;
 
 module.exports = function getExif(...args) {
 	const argLen = args.length;
@@ -24,7 +25,9 @@ module.exports = function getExif(...args) {
 
 		if (buf.length === 0) {
 			const error = new RangeError(`${ERROR}, but got an empty Buffer. ${SIZE_ERROR}`);
-			error.code = 'ERR_EMPTY_DATA';
+			error.code = 'ERR_INSUFFICIENT_DATA_SIZE';
+			error.passedSize = 0;
+			error.requiredSize = MINIMUM_JPEG_SIZE;
 
 			throw error;
 		}
@@ -33,7 +36,9 @@ module.exports = function getExif(...args) {
 
 		if (buf.length === 0) {
 			const error = new RangeError(`${ERROR}, but got '' (empty string). ${SIZE_ERROR}`);
-			error.code = 'ERR_EMPTY_DATA';
+			error.code = 'ERR_INSUFFICIENT_DATA_SIZE';
+			error.passedSize = 0;
+			error.requiredSize = MINIMUM_JPEG_SIZE;
 
 			throw error;
 		}
@@ -44,11 +49,11 @@ module.exports = function getExif(...args) {
 		throw error;
 	}
 
-	if (buf.length < 107) {
+	if (buf.length < MINIMUM_JPEG_SIZE) {
 		const error = new RangeError(`${ERROR}, but got insufficient data size ${Buffer.byteLength(arg, 'binary')}. ${SIZE_ERROR}`);
 		error.code = 'ERR_INSUFFICIENT_DATA_SIZE';
 		error.passedSize = buf.length;
-		error.requiredSize = 107;
+		error.requiredSize = MINIMUM_JPEG_SIZE;
 
 		throw error;
 	}
